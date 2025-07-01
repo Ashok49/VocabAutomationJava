6b6496dc47da76ce6a1bcd2a5c6e4ed4d7eb9e45
ashok49 <ashokctn01@gmai.com>
2025-06-30 23:20:18 -0700

    Add core spring boot project files

---
+++ src/main/java/com/ashokvocab/vocab_automation/VocabAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.ashokvocab.vocab_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashokvocab/vocab_automation/VocabAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashokvocab.vocab_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
